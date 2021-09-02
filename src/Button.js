02968ce352cd82cf553f1f94f20adb03ce83bd17
Thyara <thyaranonato@outlook.com>
2021-09-02 20:08:31 -0300

    Finaliza exercicio

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import "./button.css";
 
-const Button = ({ className, children, disable, onClick }) => (
+const Button = ({ className, children, disabled, onClick }) => (
   <button
     onClick={ onClick }
     className={`button-text ${className}`}
-    disabled={ disable }
+    disabled={ disabled }
   >
     { children }
   </button>
